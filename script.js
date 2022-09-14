fa5403736d9f5c35e8a633161337512657b4aab1
Nazia <Naziasuleman901@gmail.com>
2022-09-14 23:44:25 +0500

    On pressing Enter key, submit action occurs and pop up occur

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+ 
  if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(
         function (position) {
@@ -15,33 +17,45 @@ const inputElevation = document.querySelector('.form__input--elevation');
             const { longitude } = position.coords;
             console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
             
-            var map = L.map('map').setView([51.505, -0.09], 13);
+            map = L.map('map').setView([51.505, -0.09], 13);
 
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-           map.on('click', function(mapEvent){
-              console.log(mapEvent);
-              const {lat, lng} = mapEvent.latlng;
-
-              L.marker([lat, lng]).addTo(map)
-              .bindPopup(L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: 'running-popup', 
-
-              }))
-              .setPopupContent('workout')
-              .openPopup();
-             })
-
-             
-            
-        },
+           map.on('click', function(mapE){
+            mapEvent = mapE;
+            form.classList.remove('hidden');
+            inputDistance.focus();
+              
+             })   
+       },
         function () {
           alert('Could not get your position');
         }
       );
+
+      form.addEventListener('submit', function(e){
+        e.preventDefault(); 
+
+        
+        console.log(mapEvent);
+        const {lat, lng} = mapEvent.latlng;
+
+        L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup', 
+          })
+        )
+
+        .setPopupContent('workout')
+        .openPopup();
+      });
+
+
